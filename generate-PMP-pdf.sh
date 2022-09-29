2b8aca996bb7dcd5184f84480333286b0858ac37
Franck Silvestre <franck.silvestre@gmail.com>
2022-09-29 17:24:56 +0200

    Generation pdf

---
+++ generate-PMP-pdf.sh
@@ -0,0 +1,6 @@
+bundle exec asciidoctor \
+-r asciidoctor-pdf \
+-r asciidoctor-diagram \
+-b pdf \
+PlanManagementProjet.adoc \
+-o SAE-S3-Plan-Management-Projet.pdf
---
+++ generate-README-pdf.sh
@@ -0,0 +1,6 @@
+bundle exec asciidoctor \
+-r asciidoctor-pdf \
+-r asciidoctor-diagram \
+-b pdf \
+README.adoc \
+-o SAE-S3-Presentation-Generale.pdf
